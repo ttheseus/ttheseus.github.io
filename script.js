b9781605de4445509e151dd210f336dc95d7541e
ttheseus__ <113858167+ttheseus@users.noreply.github.com>
2025-02-10 15:48:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,49 @@
 function updateTime() {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
-  
-    const timeString = `${hours}:${minutes}:${seconds}`;
-    document.getElementById('current-time').textContent = timeString;
-  }
-  
-  // Update time immediately and then every second
-  updateTime();
-  setInterval(updateTime, 1000);
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, '0');
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  const seconds = now.getSeconds().toString().padStart(2, '0');
 
-  function updateDate() {
-    const now = new Date();
-    const day = now.getDate().toString().padStart(2, '0');
-    const month = now.getMonth().toString().padStart(2, '0');
-    const year = now.getFullYear().toString().padStart(4, '0');
+  const timeString = `${hours}:${minutes}:${seconds}`;
+  document.getElementById('current-time').textContent = timeString;
+}
 
-    const dayString = `${year}-${month}-${day}`;
-    document.getElementById('current-date').textContent = dayString;
-  }
+// Update time immediately and then every second
+updateTime();
+setInterval(updateTime, 1000);
 
-  function timeUntilNextMidnight() {
-    const now = new Date();
-    const nextMidnight = new Date(now);
-    nextMidnight.setHours(24, 0, 0, 0);
+function updateDate() {
+  const now = new Date();
+  const day = now.getDate().toString().padStart(2, '0');
+  const month = now.getMonth().toString().padStart(2, '0');
+  const year = now.getFullYear().toString().padStart(4, '0');
 
-    return nextMidnight - now;
-  }
+  const dayString = `${year}-${month}-${day}`;
+  document.getElementById('current-date').textContent = dayString;
+}
 
+function timeUntilNextMidnight() {
+  const now = new Date();
+  const nextMidnight = new Date(now);
+  nextMidnight.setHours(24, 0, 0, 0);
+
+  return nextMidnight - now;
+}
+
+updateDate();
+
+setTimeout(function () {
   updateDate();
+  setInterval(updateDate, 24 * 60 * 60 * 1000);
+}, timeUntilNextMidnight());
 
-  setTimeout(function() {
-    updateDate();
-    setInterval(updateDate, 24*60*60*1000);
-  }, timeUntilNextMidnight());
-  
+function navigateToPage(page) {
+  window.location.href = page;
+}
+
+
+var typeElement = document.querySelector('.type');
+if (typeElement) {
+  // Show the element after the page loads
+  typeElement.style.display = 'inline-block';
+}
