b32bbb7e439df957ee1212fcd6dd557cfe8da5da
ttheseus__ <113858167+ttheseus@users.noreply.github.com>
2025-03-24 11:02:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -47,3 +47,65 @@ if (typeElement) {
   // Show the element after the page loads
   typeElement.style.display = 'inline-block';
 }
+
+function aboutMeText(section) {
+  const textContent = document.getElementById('text-content')
+  const navContent = document.getElementById('nav-content')
+
+  textContent.classList.add('fade-in')
+
+  setTimeout(() => {
+    textContent.classList.remove('fade-in')
+  }, 1000);
+
+  if (section === 'hobbies') {
+    textContent.innerHTML = `
+      <p> 
+      > digital illustration <br>
+      > creative writing <br>
+      > coding games <br>
+      > cryptography & hacking <br>
+      > volleyball 
+      </p>
+    `
+    navContent.innerHTML = `
+      &#9733 <a href="#" onclick="aboutMeText('back')">back... </a> <br><br>
+      &#9733 <a href="#" onclick="aboutMeText('musings')">musings...</a> <br><br>
+      &#9733 <a href="#">fun... !!!</a>
+    `
+  } else if (section === 'musings') {
+    textContent.innerHTML = `
+      <p>what did the object detector say when it went skydiving?</p>
+      <input type="text" id="joke" name="joke">  
+      <button onclick="checkJoke()">submit</button>
+      <p id="response"></p>
+    `
+    navContent.innerHTML = `
+      &#9733 <a href="#" onclick="aboutMeText('hobbies')">hobbies...</a> <br><br>
+      &#9733 <a href="#" onclick="aboutMeText('back')">back... </a> <br><br>
+      &#9733 <a href="#">fun... !!!</a>
+    `
+  } else if (section == 'back') {
+    textContent.innerHTML = `
+      &#9733 computer engineer at the <a href="https://uwaterloo.ca/">university of waterloo</a> !<br> <br>
+      &#9733 she/her | &#9733 ace | &#9733 <u>artist</u> & aspiring <u>dev</u> &#9733 <br><br>
+      &#9733 hoping to have a career in <u>ai development</u> :P
+    `
+    navContent.innerHTML = `
+      &#9733 <a href="#" onclick="aboutMeText('hobbies')">hobbies... </a> <br><br>
+      &#9733 <a href="#" onclick="aboutMeText('musings')">musings...</a> <br><br>
+      &#9733 <a href="#">fun... !!!</a>
+    `
+  }
+}
+
+function checkJoke() {
+  const input = document.getElementById('joke').value.trim().toLowerCase();
+  const response = document.getElementById('response');
+
+  if (input == 'yolo') {
+    response.textContent = 'you are .... CORRECT!!! have a cookie';
+  } else {
+    response.textContent = 'you are .... WRONG!!! airstrike in 3... 2... 1... jk'
+  }
+}
